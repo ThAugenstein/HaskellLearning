d89d8c25e75eda6f72b18223e860c32d99a31b4b
Thomas Augenstein <t.augenstein@mac.com>
2024-01-21 15:32:47 +0100

    Add Lazy evaluation (example)

--- src/basics.hs
+++ src/basics.hs
@@ -17,3 +17,17 @@ sum1 (x : xs) = x + sum1 xs
 
 sum2 :: (Num a) => [a] -> a
 sum2 = foldr (+) 0
+
+-- Lazy evaluation
+
+func1 arg = undefined
+func2 arg = undefined
+func3 arg = undefined
+
+-- Evaluates just one of func1 and func2 depending of value of z
+
+myfunc arg =
+    let x = func1 arg
+        y = func2 arg
+        z = func3 arg
+     in if z then x else y
